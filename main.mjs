78855b3faa46abfe651ef50eba323a8b6361e3d7
anjafu <anjafu@uia.no>
2026-03-26 19:27:54 +0100

    Added comments and regions for more readability

--- main.mjs
+++ main.mjs
@@ -1,3 +1,4 @@
+//#region Server variables
 const baseURL = "https://alchemy-kd0l.onrender.com";
 const regURL = `${baseURL}/start`;
 const statusURL = `${baseURL}/status`;
@@ -13,6 +14,8 @@ const requestHeaders = {
     "Authorization": token,
 }
 
+//#endregion
+//#region Challenge 3
 const symbolDictonary = {
     "☉" : "Gold",
     "☿" : "Quicksilver",
@@ -20,55 +23,67 @@ const symbolDictonary = {
     "♂" : "Iron"
 }
 
-function decode(code){
-    let decodedCode = "";
+//decodes a code based on a dictionary
+function decodeWithDictionary(code, dictionary){
+    let output = "";
 
-    for(let letter of code){
-        let decodedLetter = symbolDictonary[letter];
-        decodedCode += decodedLetter;
+    for(let char of code){
+        let decodedLetter = dictionary[char];
+        output += decodedLetter;
     }
 
-    return decodedCode;
+    return output;
 }
+//#endregion
 
-function findBigLetters(text){
-    let onlyBigLetters = "";
+//#region Challenge 4
+//finds the big letters in a string
+function findBigLetters(string){
+    let output = "";
 
-    for(let letter of text){
-        //checks if letter is the same as uppercase and that its a letter and not symbol/space
-        if(letter == letter.toUpperCase() && letter.match(/[a-z]/i)){
-            onlyBigLetters += letter;
+    for(let char of string){ //loops through each character in a string
+        //checks if character is the same as uppercase and that its a letter and not symbol/space
+        if(char == char.toUpperCase() && char.match(/[a-z]/i)){
+            output += char; //if big letter -> add to output
         }
     }
     
-    return onlyBigLetters;
+    return output;
 }
+//#endregion
 
+//#region Challenge 5
+//all the different gates done with one line functions for simplicity (checked tables online to see which values should be 1)
 const andGate = (a, b) => { if(a == 1 && b == 1) {return 1} else {return 0}};
 const orGate = (a, b) => { if(a == 1 || b == 1) {return 1} else {return 0}};
 const notGate = (a) => { if(a == 0) {return 1} else {return 0}};
-const andNotGate = (a, b) =>  notGate(andGate(a,b));
+const andNotGate = (a, b) =>  notGate(andGate(a,b)); // not needed to make, but makes out job easier
 const xOrGate = (a, b) => { if((a == 1 && b == 0) || (a == 0 && b == 1)) {return 1} else {return 0}};
 
+//runs a curcuit (only works for the one made in the challenge)
 function curcuit(a,b,c,d,e){
-    const outputA = orGate(andGate(a,b), notGate(c));
+    const outputA = orGate(andGate(a,b), notGate(c)); //starts with the last gate "orGate" and then the other ones
     const outputB = andNotGate(xOrGate(c,d), andGate(d,e));
 
-    return "" + outputA + outputB;
+    return "" + outputA + outputB; //makes it into a string
 }
 
-function runStringInChunks(string, chunkSize){
-    let fiveCharactersArray = [];
+//takes a string and splits it into an array of chunk sizes
+function splitStringIntoChunks(string, chunkSize){
+    let output = [];
 
-    for (let i = 0; i < string.length; i += chunkSize) {
-        if(string.length >= i + chunkSize){
-            fiveCharactersArray.push(string.slice(i, i + chunkSize));
+    for (let i = 0; i < string.length; i += chunkSize) { //i increases with chunk size, not by ++ (0 -> 5 -> 10 etc)
+        if(string.length >= i + chunkSize){ //only adds chunk if the string has enough characters left for desired chunk size
+            output.push(string.slice(i, i + chunkSize)); //adds characters from string in chunkSize
         }
     }
 
-    return fiveCharactersArray;
+    return output;
 }
+//#endregion
 
+
+//#region Challenge 6
 //deciphers a code as long as u have a key and the code to decipher
 //NOTE: does not work when we use regular key as key = 2, only when key is whole alphabet
 //could make key = number and then make let cipheredAlphabet = whatever it is when u push it by a number
@@ -109,6 +124,7 @@ function properCaseString(string){
 
     return output;
 }
+//#endregion
 
 // ANSI escape codes for text color
 const red = '\x1b[31m';
@@ -120,7 +136,10 @@ const lightPurple = "\x1b[38;2;220;110;250m";
 const pink = "\x1b[38;2;255;51;153m";
 const reset = '\x1b[0m';
 
+//#region Init program
+//function that starts the whole program
 async function init(){
+    //step 1: log in to server
     await logInToServer(credentials) ? 
     console.log(cyan + "Log in successful \n") : console.log(red + "haha loser \n");
 
@@ -130,7 +149,7 @@ async function init(){
 
     //question 3:
     //let codeword = questionData.prompt.split("“")[1].split("”")[0];
-    //let answerQuestion3 = decode(codeword);
+    //let answerQuestion3 = decodeWithDictionary(codeword);
     //console.log(answerQuestion3);
     //console.log(await sendAnswer(answerQuestion3));
 
@@ -142,7 +161,7 @@ async function init(){
     //question 5:
     /*
     let digits = questionData.prompt.split('"')[1].split('"')[0];
-    const digitsFiveChunked = runStringInChunks(digits, 5);
+    const digitsFiveChunked = splitStringIntoChunks(digits, 5);
     console.table(digitsFiveChunked);
     let answerToQuestion5 = "";
 
@@ -152,7 +171,7 @@ async function init(){
 
     console.log(await sendAnswer(answerToQuestion5));*/
 
-    //show question for user:
+    //step 2: show question for user:
     let questionData = await getCurrentQuestion();
     console.log("\x1b[1m\x1b[4m" + purple + "Question " + questionData.challengeId + ":" + reset);
     console.log(lightPurple + questionData.prompt);
@@ -160,7 +179,7 @@ async function init(){
     console.log(pink + "Current score: " + questionData.currentScore);
     console.log(reset + "\n" );
     
-    //send answer
+    //step 3: send answer
     let notesURL = "https://alchemy-kd0l.onrender.com/notes.md";
     let noteURL = "https://alchemy-kd0l.onrender.com/strangeNote.txt";
     let note = await (await fetch(notesURL)).text();
@@ -171,8 +190,10 @@ async function init(){
     
 }
 
-init();
+init(); //start program
+//#endregion
 
+//#region Contact server
 async function logInToServer(credentials){
     let response = await fetch(regURL, {
         method: "POST",
@@ -180,14 +201,14 @@ async function logInToServer(credentials){
         body:JSON.stringify(credentials)
     }); 
 
-    if(response.status < 300){
+    if(response.status < 300){ //if getting a response from server went OK
         response = await response.json();
         token = response.token;
         requestHeaders.Authorization = token;
 
-        return true;
+        return true; //log in successful
     } else {
-        return false;
+        return false; //bad response -> log in not successful
     }
 }
 
@@ -233,3 +254,4 @@ async function getClue(){
 
     return response;
 }
+//#endregion
